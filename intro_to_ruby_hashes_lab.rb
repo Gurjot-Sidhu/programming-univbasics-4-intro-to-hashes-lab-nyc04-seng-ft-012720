6edfa4dbef9a453e36b79e21dc3030820cfd064c
Gurjot Sidhu <gurjotsidhu4@gmail.com>
2020-01-21 00:32:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,7 +6,7 @@ end
 
 def my_hash
   my_hash ={
-    name: sam
+    name: Sam
   }
 end
 
@@ -17,15 +17,20 @@ def pioneer
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  gen = {
+    id: 4
+  }
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  hash{
+    key:value
+  }
 end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
+  
 end
 
 def update_counting_hash(hash, key)
